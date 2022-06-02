d058ef071161b47986ba70682579035cf06ef2e8
Ada <byjean94@gmail.com>
2022-06-02 15:27:17 +0200

    Added Click Handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
+import { getRandomNumber } from './Components/GetRandomNumber';
 
 function App() {
+
+  const [number, setNumber] = useState()
+  const [arr, setArray] = useState([])
+
+  const handleClick = () => {
+    setNumber(getRandomNumber(1, 49));
+    setArray(arr => [...arr, number])
+  }
+  console.log(arr)
+
   return <div className={styles.bodyPage}>
     <div className={styles.header}>
       <span className={styles.headline}>Generator Liczb Lotto</span>
@@ -9,15 +21,17 @@ function App() {
     </div>
     <div className={styles.randomDiv}>
       <div className={styles.div}>
-        <div className={styles.balls}></div>
-        <div className={styles.balls}></div>
-        <div className={styles.balls}></div>
-        <div className={styles.balls}></div>
-        <div className={styles.balls}></div>
-        <div className={styles.balls}></div>
+        <div className={styles.balls}>{arr[1]}</div>
+        <div className={styles.balls}>{arr[2]}</div>
+        <div className={styles.balls}>{arr[3]}</div>
+        <div className={styles.balls}>{arr[4]}</div>
+        <div className={styles.balls}>{arr[5]}</div>
+        <div className={styles.balls}>{arr[6]}</div>
       </div>
       <br />
-      <button>Losuj liczbę!</button>
+      <button onClick={handleClick} disabled={arr.length <= 6 ? false : true}>
+        Losuj liczbę  !
+      </button>
     </div>
   </div>
 }
