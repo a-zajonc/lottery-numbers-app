7f396b3c7b1d0e9fbbbbdfe3db7175b3d7e56301
Ada <byjean94@gmail.com>
2022-06-07 13:17:48 +0200

    Added Uniqueness of Random Numbers

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import styles from './App.module.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { getRandomNumber } from './Components/GetRandomNumber';
 
 function App() {
 
-  const [number, setNumber] = useState()
-  const [arr, setArray] = useState([])
+  const [number, setNumber] = useState([])
 
   const handleClick = () => {
-    setNumber(getRandomNumber(1, 49));
-    setArray(arr => [...arr, number])
+    setNumber(arr => [...arr, getRandomNumber(1, 49, number)]);
+
   }
-  console.log(arr)
 
   return <div className={styles.bodyPage}>
     <div className={styles.header}>
       <span className={styles.headline}>Generator Liczb Lotto</span>
-      <br />
-      <span>Twój własny chybił-trafił!</span>
     </div>
     <div className={styles.randomDiv}>
       <div className={styles.div}>
-        <div className={styles.balls}>{arr[1]}</div>
-        <div className={styles.balls}>{arr[2]}</div>
-        <div className={styles.balls}>{arr[3]}</div>
-        <div className={styles.balls}>{arr[4]}</div>
-        <div className={styles.balls}>{arr[5]}</div>
-        <div className={styles.balls}>{arr[6]}</div>
+        <div className={styles.balls}>{number[0]}</div>
+        <div className={styles.balls}>{number[1]}</div>
+        <div className={styles.balls}>{number[2]}</div>
+        <div className={styles.balls}>{number[3]}</div>
+        <div className={styles.balls}>{number[4]}</div>
+        <div className={styles.balls}>{number[5]}</div>
       </div>
       <br />
-      <button onClick={handleClick} disabled={arr.length <= 6 ? false : true}>
+      <button onClick={handleClick}
+        disabled={number.length < 6 ? false : true}
+      >
         Losuj liczbę  !
       </button>
     </div>
--- src/Components/GetRandomNumber.js
+++ src/Components/GetRandomNumber.js
@@ -1,3 +1,12 @@
-export function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+export function getRandomNumber(min, max, array) {
+    let random = Math.floor(Math.random() * (max - min)) + min
+    if (array.length < 1) {
+        return random
+    }
+
+    if ([...array].includes(random)) {
+        random = getRandomNumber(min, max, array);
+    }
+    return random
 }
+
